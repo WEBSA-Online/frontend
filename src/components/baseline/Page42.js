b4300f3cd47bd3f04a4d868f50d3b8e0c11fe215
bakersen <bakersen@gmail.com>
2023-08-03 15:30:02 +0300

    Fixed grammar errors

--- src/components/baseline/Page42.js
+++ src/components/baseline/Page42.js
@@ -166,7 +166,7 @@ const Page42 = () => {
 		>
 			<Container fixed>
 				<h1 style={{ lineHeight: "30px" }}>{details.question}</h1>
-				<h2>(You can choose up to 3)</h2>
+				<h2>(You can choose all that applies)</h2>
 				<Grid container spacing={2}>
 					{details.row1.map((value, index) => {
 						return (
